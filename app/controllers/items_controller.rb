58013f570addafbf4ace34f643855fe5dd6fc39a
moses ngige <moses32ngigz@gmail.com>
2022-09-27 13:25:10 +0300

    commit tree f821512ad8759f165eb059dbd5548c95cc53a03e

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,50 @@
 class ItemsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
   def index
-    items = Item.all
+    if params[:user_id]
+      user = find_user
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
+
+  def show
+    item = find_item
+    render json: item 
+  end
+  def create 
+    user = find_user
+    item = user.items.create(item_params)
+    render json: item , status: 201
+
+
+  end
+
+
+
+   
+  private
+ 
+  def record_not_found
+    render :text => "404: Resource not found", :status => 404
+  end
+
+  def find_user
+ User.find(params[:user_id])
+  end
+
+  def find_item
+    Item.find(params[:id])
+  end
+
+  def item_params
+    params.permit(:name,:description,:price)
+  end
+
+
+
+
 
 end
